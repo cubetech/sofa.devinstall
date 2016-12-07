ea5a0f24150df0371ae1f938c8bf3359f46ff3ce
Christoph S. Ackermann <christoph.ackermann@cubetech.ch>
2016-12-07 12:35:34 +0100

    fixed bin path, added salts generation

--- cubetech.sh
+++ cubetech.sh
@@ -4,15 +4,16 @@ if [ -f ~/.ctconfig ]; then
 	source ~/.ctconfig
 	echo "Project: "
 	echo ${PWD##*/}
-	../../bin/wp package install aaemnnosttv/wp-cli-dotenv-command
+	vendor/bin/wp package install aaemnnosttv/wp-cli-dotenv-command
 	cp .env.example .env
-	../../bin/wp dotenv set DB_NAME `echo $CTUSER`_`echo ${PWD##*/}`
-	../../bin/wp dotenv set DB_USER `echo $CTUSER`
-	../../bin/wp dotenv set DB_PASSWORD \"`echo $CTPW`\"
-	../../bin/wp dotenv set DB_HOST localhost
-	../../bin/wp dotenv set WP_HOME http://${PWD##*/}.$CTUSER.dev.cubetech.ch
-	../../bin/wp db create --quiet
-	../../bin/wp core install --url=http://${PWD##*/}.$CTUSER.dev.cubetech.ch --title=${PWD##*/} --admin_user=$CTWPUSER --admin_password=$CTWPPW --admin_email=$CTWPMAIL
+	vendor/bin/wp dotenv set DB_NAME `echo $CTUSER`_`echo ${PWD##*/}`
+	vendor/bin/wp dotenv set DB_USER `echo $CTUSER`
+	vendor/bin/wp dotenv set DB_PASSWORD \"`echo $CTPW`\"
+	vendor/bin/wp dotenv set DB_HOST localhost
+	vendor/bin/wp dotenv set WP_HOME http://${PWD##*/}.$CTUSER.dev.cubetech.ch
+	vendor/bin/wp db create --quiet
+	vendor/bin/wp core install --url=http://${PWD##*/}.$CTUSER.dev.cubetech.ch --title=${PWD##*/} --admin_user=$CTWPUSER --admin_password=$CTWPPW --admin_email=$CTWPMAIL
+	vendor/bin/wp dotenv salts regenerate
 else
 	echo "No cubetech config available."
 	exit 0
