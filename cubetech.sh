08c3e34c2a08937629ccde54caf8f1728c24d6ce
Christoph S. Ackermann <christoph.ackermann@cubetech.ch>
2016-12-07 12:17:20 +0100

    fixed paths in scripts

--- cubetech.sh
+++ cubetech.sh
@@ -2,15 +2,17 @@
 
 if [ -f ~/.ctconfig ]; then
 	source ~/.ctconfig
-	vendor/bin/wp package install aaemnnosttv/wp-cli-dotenv-command
+	echo "Project: "
+	echo ${PWD##*/}
+	../../bin/wp package install aaemnnosttv/wp-cli-dotenv-command
 	cp .env.example .env
-	vendor/bin/wp dotenv set DB_NAME `echo $CTUSER`_`echo ${PWD##*/}`
-	vendor/bin/wp dotenv set DB_USER `echo $CTUSER`
-	vendor/bin/wp dotenv set DB_PASSWORD \"`echo $CTPW`\"
-	vendor/bin/wp dotenv set DB_HOST localhost
-	vendor/bin/wp dotenv set WP_HOME http://${PWD##*/}.$CTUSER.dev.cubetech.ch
-	vendor/bin/wp db create --quiet
-	vendor/bin/wp core install --url=http://${PWD##*/}.$CTUSER.dev.cubetech.ch --title=${PWD##*/} --admin_user=$CTWPUSER --admin_password=$CTWPPW --admin_email=$CTWPMAIL
+	../../bin/wp dotenv set DB_NAME `echo $CTUSER`_`echo ${PWD##*/}`
+	../../bin/wp dotenv set DB_USER `echo $CTUSER`
+	../../bin/wp dotenv set DB_PASSWORD \"`echo $CTPW`\"
+	../../bin/wp dotenv set DB_HOST localhost
+	../../bin/wp dotenv set WP_HOME http://${PWD##*/}.$CTUSER.dev.cubetech.ch
+	../../bin/wp db create --quiet
+	../../bin/wp core install --url=http://${PWD##*/}.$CTUSER.dev.cubetech.ch --title=${PWD##*/} --admin_user=$CTWPUSER --admin_password=$CTWPPW --admin_email=$CTWPMAIL
 else
 	echo "No cubetech config available."
 	exit 0
